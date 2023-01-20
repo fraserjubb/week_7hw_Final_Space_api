5d9ddaa7930da66470d42acaff5282a4c481ca5e
Fraser Jubb <fraser.jubb@gmail.com>
2023-01-20 18:52:25 +0000

    Initial set up of api is complete (everything appears to be working and able to extract info from api).

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import CharacterBox from './composite/CharacterBox';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetch("https://finalspaceapi.com/api/v0/character")
+      .then((response) => response.json())
+      // .then(characters => console.log(characters))
+      .then(characters => setCharacters(characters))
+
+  }, [])
+
+  const characterItems = characters.map(character => {
+    return <li key = {character.id}>{character.name}</li>
+  })
+
+  return(
+  <>
+  <CharacterBox></CharacterBox>
+  <ul>{characterItems}</ul>
+  </>
+)}
 
 export default App;
